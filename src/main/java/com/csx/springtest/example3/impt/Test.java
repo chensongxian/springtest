c39055dfd51d43218b04b8f30de01b8d0b2e20ce
chensongxian <1164077611@qq.com>
2018-02-07 18:05:48 +0800

    提交未提交的

---
+++ src/main/java/com/csx/springtest/example3/impt/Test.java
@@ -0,0 +1,27 @@
+package com.csx.springtest.example3.impt;
+
+import com.csx.springtest.example3.fb.Car;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: TODO
+ * @Author: csx
+ * @Date: 2018/02/07
+ */
+public class Test {
+    /**
+     * 整合多个配置文件
+     * @param args
+     */
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("com.csx.springtest.example3.impt/beans2.xml");
+
+        Car car1 = context.getBean("car1", Car.class);
+
+        Assertions.assertNotNull(car1);
+    }
+}
