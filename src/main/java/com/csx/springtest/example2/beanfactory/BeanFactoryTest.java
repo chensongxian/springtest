41d5250963bbd65b504c79389690427af2c3689f
chensongxian <1164077611@qq.com>
2018-11-19 17:14:08 +0800

    'IOC容器的加载'

--- src/main/java/com/csx/springtest/example2/beanfactory/BeanFactoryTest.java
+++ src/main/java/com/csx/springtest/example2/beanfactory/BeanFactoryTest.java
@@ -23,14 +23,19 @@ public class BeanFactoryTest {
     @Test
     public void getBean() throws IOException {
         ResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
+        // 1. 获取资源
         Resource resource = resolver.getResource("classpath:com/csx/springtest/example2/beans.xml");
 
         System.out.println(resource.getURL());
 
+        // 2. 获取BeanFactory
         DefaultListableBeanFactory listableBeanFactory=new DefaultListableBeanFactory();
 
+        // 3. 根据新建的 BeanFactory 创建一个 BeanDefinitionReader 对象，该 Reader 对象为资源的解析器
         XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(listableBeanFactory);
+        XmlBeanDefinitionReader reader2=new XmlBeanDefinitionReader(listableBeanFactory);
 
+        // 4. 装载资源
         reader.loadBeanDefinitions(resource);
 
         System.out.println(" init bean");
