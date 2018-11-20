76699cc4e31b0c822e631763d5b5b1588f43ff8c
chensongxian <1164077611@qq.com>
2018-11-20 10:14:52 +0800

    '实现主从和分库逻辑'

---
+++ src/main/java/com/csx/springtest/example1/dbrouter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Package com.csx.springtest.example1.dbrouter
+ * @Description: 实现主从和分库逻辑
+ * @author csx
+ * @date 2018/11/20 0020
+ */
+package com.csx.springtest.example1.dbrouter;
---
+++ src/main/java/com/csx/springtest/example3/ditype/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Package com.csx.springtest.example3.ditype
+ * @Description: TODO
+ * @author csx
+ * @date 2018/11/20 0020
+ */
+package com.csx.springtest.example3.ditype;
